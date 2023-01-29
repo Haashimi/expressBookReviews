e42f479209fa0dccd475ba2ad121efb7feeaec2a
Haashimi <mohameddiini@outlook.com>
2023-01-29 16:27:14 +0300

    final course project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -52,51 +52,33 @@ function getByISBN(isbn) {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
   getBooks().then((bks) => res.send(JSON.stringify(bks)));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
   getByISBN(req.params.isbn)
   .then(
       result => res.send(result.reviews),
-      error => res.status(error.status).json({message: error.message})
-  );
-
-   
+      error => res.status(error.status).json({message: error.message}));   
  });
  
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  const author = req.params.author;
-
-  
+  const author = req.params.author;  
   getBooks()
   .then((bookEntries) => Object.values(bookEntries))
   .then((books) => books.filter((book) => book.author === author))
   .then((filteredBooks) => res.send(filteredBooks));
 
-  //return res.status(300).json({message: "Yet to be implemented"});
-
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
   getBooks()
   .then((bookEntries) => Object.values(bookEntries))
   .then((books) => books.filter((book) => book.title === title))
   .then((filteredBooks) => res.send(filteredBooks));
-    
-
-   
-
-
-  //return res.status(300).json({message: "Yet to be implemented"});
-   
 });
 
 //  Get book review
